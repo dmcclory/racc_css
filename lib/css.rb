6043eaa108b2a3c0bee5f0540c920aca0e610ea2
Dan McClory <danmcclory@gmail.com>
2013-08-04 14:41:21 -0400

    traverse_select, build a boolean lambda from the selector

--- lib/css.rb
+++ lib/css.rb
@@ -5,8 +5,23 @@ module Css
 
   extend self
 
+  def parse_and_interpret(selector)
+    lambda { |e| e.name == selector.chars.to_a.last }
+  end
+
+  def traverse_select(document, &block)
+    array = []
+    document.traverse {|e|
+      array << e if block.call(e)
+    }
+    return array
+  end
+
   def select(document, selector)
-    document.to_s.scan(selector.chars.to_a.last)
+    selector_func = parse_and_interpret(selector)
+    traverse_select(document) { |e| 
+      selector_func.call(e)
+    }
   end
 
 end
