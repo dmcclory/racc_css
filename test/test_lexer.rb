a3432a7d815cc9ba5c96724b4a9857cd2ee3f900
Dan McClory <danmcclory@gmail.com>
2013-08-04 18:05:46 -0400

    add failure message to assert_tokenizes_to

--- test/test_lexer.rb
+++ test/test_lexer.rb
@@ -8,7 +8,7 @@ class CssLexerTest < MiniTest::Unit::TestCase
 
   def assert_tokenizes_to selector, expect
     result = Css::Lexer.new.tokens(selector)
-    assert_equal result, expect
+    assert_equal expect, result, "expected selector #{selector} to tokenize to #{expect.inspect}, but was #{result.inspect}"
   end
 
   def test_names_get_name
