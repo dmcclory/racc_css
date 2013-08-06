3ac37e7dfdcf8f5da374b7f092953a8fbd9bc036
Dan McClory <danmcclory@gmail.com>
2013-08-05 23:35:39 -0400

    make all of the descendant selector assertion consistent. checking count, for now

--- test/test_descendant_selector.rb
+++ test/test_descendant_selector.rb
@@ -16,46 +16,46 @@ class DescendantSelectorTest < MiniTest::Unit::TestCase
   def test_can_select_different_numbers_of_B_descendants_of_A
     doc = Nokogiri.XML "<a><b/><b/><b/></a>"
     selector = "a b"
-    assert Css.select(doc, selector).count == 3
+    assert_equal 3, Css.select(doc, selector).count
   end
 
   def test_can_select_different_numbers_of_C_descendants_of_A
     doc = Nokogiri.XML "<a><c/><c/><c/></a>"
     selector = "a c"
-    assert Css.select(doc, selector).count == 3
+    assert_equal 3, Css.select(doc, selector).count
   end
 
   def test_can_select_names_as_descendants
     doc = Nokogiri.XML "<a><cat/><cat/><cat/></a>"
     selector = "a cat"
-    assert Css.select(doc, selector).count == 3
+    assert 3, Css.select(doc, selector).count
   end
 
   def test_can_actually_select_descendants
     doc = Nokogiri.XML "<root><a><b/><b/></a><b/></root>"
     selector = "a b"
-    assert Css.select(doc, selector).count == 2
+    assert_equal 2, Css.select(doc, selector).count
   end
 
   def test_can_chain_descendant_seletors
     doc = Nokogiri.XML "<root><a><b><c/></b><d><c/></d></a></root>"
     selector = "a b c"
     result = Css.select(doc, selector)
-    assert_equal result.count, 1
+    assert_equal 1, result.count
   end
 
   def test_descendant_selectors_can_be_nested_arbitrarily_deeply
     doc = Nokogiri.XML "<root><a><b><c><d><e><f/></e></d></c></b></a></root>"
     selector = "a b c d e f"
     result = Css.select(doc, selector)
-    assert_equal result.count, 1
+    assert_equal 1, result.count
   end
 
   def test_irrelevant_elements_are_ignored_in_descendant_selector
     doc = Nokogiri.XML "<a><b><c/></b></a>"
     selector = "a c"
     result = Css.select(doc, selector)
-    assert_equal 1, result.count, "Expected 1 element, but got #{result.count}"
+    assert_equal 1, result.count
   end
 
 end
