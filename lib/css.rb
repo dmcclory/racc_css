a84bb0d1b9cfb45e79cb3f894588dfba87fdf26e
Dan McClory <danmcclory@gmail.com>
2013-08-05 22:13:09 -0400

    use the lexer, parser and interpreter in CSS.select

--- lib/css.rb
+++ lib/css.rb
@@ -1,24 +1,30 @@
 require 'nokogiri'
 require 'css.rex.rb'
 require 'css.tab.rb'
+require 'css_interpreter'
 
 module Css
 
   extend self
 
+  @@lexer = Lexer.new
+  @@parser = Parser.new
+  @@interpreter = Interpreter.new
+
+  
   def select(document, selector)
     selector_func = parse_and_interpret(selector)
-    traverse_select(document) { |e| 
+    traverse_select(document.root) { |e| 
       selector_func.call(e)
     }
   end
 
   def parse_and_interpret(selector)
-    names = selector.split(" ").reverse
-    lambda { |e| 
-      ancestor_names = e.ancestors.map {|a| a.name }.push e.name
-      names.all? { |n| ancestor_names.include? n }
-    }
+    tokens = @@lexer.tokens(selector)
+    tree = @@parser.parse tokens
+    function = @@interpreter.interpret tree
+
+    return function
   end
 
   def traverse_select(document, &block)
