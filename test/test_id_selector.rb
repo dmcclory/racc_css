76690f03196430983c93086d0eaca594cfdf4729
Dan McClory <danmcclory@gmail.com>
2013-08-13 16:05:49 -0400

    add test for bare id selector

--- test/test_id_selector.rb
+++ test/test_id_selector.rb
@@ -5,13 +5,15 @@ require 'minitest/autorun'
 
 class IdSelectorTests < MiniTest::Unit::TestCase
 
-  def setup
-
-  end
-
   def test_can_select_element_with_an_id
     doc = Nokogiri.XML "<root><foo><a id='so_cool'/></foo></root>"
     result = Css.select doc, 'a#so_cool'
     assert_equal 'a', result.first.name
   end 
+
+  def test_can_have_bare_id
+    doc = Nokogiri.XML "<root><a><b id='brydon'/></a></root>"
+    result = Css.select doc, '#brydon'
+    assert_equal 'b', result.first.name
+  end
 end
